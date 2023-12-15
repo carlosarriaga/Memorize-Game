823895306bb3ded2e2eb5e66826fc25c4452574b
carlos.arriaga <carlos.arriaga@live.com>
2023-12-14 20:30:38 -0600

    The logic of the game is created!

--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -7,16 +7,18 @@
 
 import Foundation
 
-class EmojiMemoryGame {
+//Makes the created objects observable by the views
+class EmojiMemoryGame: ObservableObject {
     //static - so as not to depend on an instance to access it
     static var emojis = ["🚀","🚁","🏎","🚒","🚗","🚕","🚌","🚓","🚑","🚐","🚜","🛵","🚙","🚎","🛻","🚚","🚛","🛸","⛵️","✈️"]
     
     //model - will be the connection with our model
-    private var model: MemoryGame<String> = createMemoryGame()
+    //@Published - Ensures that observers are notified of any changes to this property.
+    @Published private var model: MemoryGame<String> = createMemoryGame()
     
     //Just the connection with the model Cards
     var cards: Array<MemoryGame<String>.Card> {
-        return model.cards
+        model.cards
     }
     
     //The deck to be played is created
@@ -25,5 +27,11 @@ class EmojiMemoryGame {
         MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in //pairIndex comes from MemoryGame.init
             emojis[pairIndex] //return the emoji for the card in turn
         }
+    }
+    
+    // MARK: Intent
+    //Connects to the function of the model
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card)
     }
 }
