212be1aeb3a8f706ccbfa2a5677102fc32902479
carlos.arriaga <carlos.arriaga@live.com>
2024-02-12 16:18:53 -0600

    We apply 3D rotation to the cards when they are opened. Animated card dealing Used viewModifiers and animatableModifiers Clock animation

--- Memorize/Memorize/Cardify.swift
+++ Memorize/Memorize/Cardify.swift
@@ -10,21 +10,35 @@ import SwiftUI
 // Will be in charge of creating the card,
 //will place both sides of the card (open/closed).
 //She will also modify if the card is flipped over.
-struct Cardify: ViewModifier {
-    var isFaceUp: Bool
+struct Cardify: AnimatableModifier {
+    
+    var rotation: Double //indicates how many degrees the card is rotated
+    
+    
+    //rotation binds to animatableData and takes care of the animation state
+    var animatableData: Double {
+        get { rotation }
+        set { rotation = newValue }
+    }
+    
+    //Changes the grades depending on whether it is face up or not.
+    init(isFaceUp: Bool) {
+        rotation = isFaceUp ? 0 : 180
+    }
     
     func body(content: Content) -> some View {
         ZStack {
             let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
-            if isFaceUp {
+            if rotation < 90 {
                 shape.fill().foregroundColor(.white)
                 shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
             } else {
                 shape.fill()
             }
             content //Here falls what wants to be "Cardified"
-                .opacity(isFaceUp ? 1 : 0) //Solve the problem of rotation 
+                .opacity(rotation < 90 ? 1 : 0) //Solve the problem of rotation
         }
+        .rotation3DEffect(Angle.degrees(rotation), axis: (0, 1, 0))
     }//body
     
     //The values of constants are added together
